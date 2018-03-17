f55b43fd09005a54a61c9043cac6eddf8ceb3090
ustal <quit.ipad2@gmail.com>
2018-03-17 18:07:18 +0200

    Fix entity manager issue and add .idea into branch

--- project/src/AppBundle/Controller/DefaultController.php
+++ project/src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Post;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -11,18 +12,12 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template()
      */
     public function indexAction(Request $request)
     {
-        $post = new Post();
-        $post->setDescription('Test description');
-        $post->setTitle('Test name');
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($post);
-        $em->flush();
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        ];
     }
 }
